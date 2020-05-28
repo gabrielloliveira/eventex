ef580ba6fefe789eb1a5d4d46b460f74c7ab34d2
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2020-05-28 12:49:28 -0300

    adicionado template para inscrição do evento

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,13 +1,16 @@
+from django.core import mail
 from django.test import TestCase
+from django.urls import reverse
+
 from .forms import SubscriptionForm
 
 
 class SubscribeTest(TestCase):
     def setUp(self):
-        self.response = self.client.get("/inscricao/")
+        self.response = self.client.get(reverse("subscriptions:subscribe"))
 
     def test_get(self):
-        """GET /inscriaco/ must return status code 200"""
+        """GET 'subscriptions:subscribe' must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
@@ -17,10 +20,10 @@ class SubscribeTest(TestCase):
     def test_html(self):
         """HTML must contain input tags"""
         self.assertContains(self.response, "<form")
-        self.assertContains(self.response, "<input", 5)
+        self.assertContains(self.response, "<input", 6)
         self.assertContains(self.response, 'type="text"', 3)
         self.assertContains(self.response, 'type="email"')
-        self.assertContains(self.response, '<button>')
+        self.assertContains(self.response, 'type="submit"')
 
     def test_csrf(self):
         """HTML must contain csrf token"""
@@ -35,3 +38,78 @@ class SubscribeTest(TestCase):
         """Form must have 4 fields"""
         form = self.response.context["form"]
         self.assertSequenceEqual(["name", "cpf", "email", "phone"], list(form.fields))
+
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = {
+            'name': 'Gabriell Oliveira',
+            'cpf': '12345678901',
+            'email': 'gabrielloliveira097@gmail.com',
+            'phone': '99-99999-9999'
+        }
+        self.response = self.client.post(reverse("subscriptions:subscribe"), data)
+
+    def test_post(self):
+        """Valid POST should redirect to 'subscriptions:subscribe'"""
+        self.assertEqual(302, self.response.status_code)
+
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expect = 'Confirmação de inscrição'
+        self.assertEqual(expect, email.subject)
+
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        expect = 'contato@eventex.com.br'
+        self.assertEqual(expect, email.from_email)
+
+    def test_subscription_email_to(self):
+        email = mail.outbox[0]
+        expect = ['contato@eventex.com.br', 'gabrielloliveira097@gmail.com']
+        self.assertEqual(expect, email.to)
+
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+        self.assertIn('Gabriell Oliveira', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('gabrielloliveira097@gmail.com', email.body)
+        self.assertIn('99-99999-9999', email.body)
+
+
+class SubscribeInvalidPost(TestCase):
+    def setUp(self):
+        self.response = self.client.post(reverse("subscriptions:subscribe"), {})
+
+    def test_post(self):
+        """Invalid POST should not redirect"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
+
+    def test_has_form(self):
+        form = self.response.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_errors(self):
+        form = self.response.context['form']
+        self.assertFalse(form.is_valid())
+
+
+class SubscribeSuccessMessage(TestCase):
+    def setUp(self):
+        data = {
+            'name': 'Gabriell Oliveira',
+            'cpf': '12345678901',
+            'email': 'gabrielloliveira097@gmail.com',
+            'phone': '99-99999-9999'
+        }
+        self.response = self.client.post(reverse("subscriptions:subscribe"), data, follow=True)
+
+    def test_message(self):
+        self.assertContains(self.response, 'Inscrição realizada com sucesso!')
+
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,29 @@
+from django.core import mail
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.urls import reverse
+
 from .forms import SubscriptionForm
 
 
 def subscribe(request):
+    if request.POST:
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            body = render_to_string("subscriptions/subscription_email.txt", form.cleaned_data)
+            mail.send_mail(
+                'Confirmação de inscrição',
+                body,
+                'contato@eventex.com.br',
+                ['contato@eventex.com.br', form.cleaned_data['email']]
+            )
+            messages.success(request, 'Inscrição realizada com sucesso!')
+            return HttpResponseRedirect(reverse("subscriptions:subscribe"))
+        else:
+            return render(request, "subscriptions/subscription_form.html", {'form': form})
+
     context = {
         'form': SubscriptionForm()
     }
